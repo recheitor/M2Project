ebe809aa33166f77276367d21e010d47c01b60b8
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-08-15 13:02:33 +0200

    Edamam API ready

--- services/edamam-service.js
+++ services/edamam-service.js
@@ -2,26 +2,19 @@ const axios = require('axios')
 
 class EdamamApiHandler {
 
-
     getIngredient(...ingredients) {
+        let ingredient = '120gr of pizza dough'
         if (typeof ingredients[0] === 'string') {
-            const apiQuery = '120gr of pizza dough AND ' + ingredients[0] + ingredients[1] + ' of ' + ingredients[2]
-            console.log(apiQuery)
-
-
+            ingredient = ' AND ' + ingredients[0] + ingredients[1] + ' of ' + ingredients[2]
         } else {
-            // const newApiQuery = ingredients[0].map(eachIngredient => {
-            //     eachIngredient
-            // })
-            console.log(ingredients)
+            for (let i = 0; i < ingredients[0].length; i++) {
+                ingredient += ' AND ' + ingredients[0][i] + ingredients[1][i] + ' of ' + ingredients[2][i]
+            }
+            console.log(ingredient)
+            return axios.get(`https://api.edamam.com/api/nutrition-data?app_id=d58513b4&app_key=0a7c7263b0b29aaebf4989eaeb262205&nutrition-type=logging&ingr=${ingredient}`)
         }
     }
-
-    // return axios.get(`https://api.edamam.com/api/nutrition-data?app_id=d58513b4&app_key=0a7c7263b0b29aaebf4989eaeb262205&nutrition-type=logging&ingr=${ingredient}`)
 }
-
-
-
 
 const edamamApi = new EdamamApiHandler()
 
