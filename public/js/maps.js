89b6837c9e685f12216ba07b9f8d1cf7b38e3a05
samuelemarino04 <samuele12601@gmail.com>
2023-08-15 11:19:14 +0200

    done

--- public/js/maps.js
+++ public/js/maps.js
@@ -9,65 +9,77 @@ function initMap() {
     infowindow = new google.maps.InfoWindow();
 }
 
-
-
-
-
-
-//COMENTARIO TEST
-
-
-
-
-//renderiza la mapa
 function renderMap() {
     myMap = new google.maps.Map(
         document.querySelector('#map'),
         { zoom: 15, center: initialCoords }
-    );
+    )
     service = new google.maps.places.PlacesService(myMap);
     for (const keyword of keywords) {
         const request = {
             location: new google.maps.LatLng(initialCoords),
-            radius: 5000,
+            radius: 7000,
             type: "restaurant",
             keyword: keyword,
-        };
+        }
+
         service.nearbySearch(request, (results, status) => {
             if (status === google.maps.places.PlacesServiceStatus.OK) {
                 for (const place of results) {
-                    const marker = new google.maps.Marker({
-                        position: place.geometry.location,
-                        map: myMap,
-                        title: place.name,
-                        // icon: photos[0].getUrl({ maxWidth: 35, maxHeight: 35 })
-                    });
-                    //info de los markers
+                    const marker = new google.maps.Marker(
+                        {
+                            position: place.geometry.location,
+                            map: myMap,
+                            title: place.name,
+                            icon: "images/marker.png"
+                        })
+
                     marker.addListener('click', () => {
-                        infowindow.setContent(`<strong>${place.name}</strong><br>${place.vicinity}<br>Rating: ${place.rating}`);
+                        infowindow.setContent(`<img src='${place.photos[0].getUrl({ maxWidth: 250, maxHeight: 200 })}' alt='No image'><br><strong>${place.name}</strong><br>${place.vicinity}<br>Rating: ${place.rating}`);
                         infowindow.open(myMap, marker);
-                    });
+                    })
                 }
             }
-        });
+        })
     }
 }
+
+
 function autoComplete() {
     const autocomplete = new google.maps.places.Autocomplete(
         document.getElementById("autocomplete")
     );
     autocomplete.addListener('place_changed', () => {
-        const selectedPlace = autocomplete.getPlace();
-        // Qui puoi fare qualcosa con il luogo selezionato, ad esempio visualizzarlo sulla mappa
-        const marker = new google.maps.Marker({
-            position: selectedPlace.geometry.location,
-            map: myMap,
-            title: selectedPlace.name,
-        });
-        myMap.setCenter(selectedPlace.geometry.location);
-        infowindow.setContent(`<strong>${selectedPlace.name}</strong><br>${selectedPlace.formatted_address}<br>Rating: ${selectedPlace.rating}`);
-        infowindow.open(myMap, marker);
-    });
+        const selectedPlace = autocomplete.getPlace()
+
+        serviceSel = new google.maps.places.PlacesService(myMap)
+
+        for (const keyword of keywords) {
+            const request = {
+                location: selectedPlace.geometry.location,
+                radius: 7000,
+                type: "restaurant",
+                keyword: keyword,
+            };
+            serviceSel.nearbySearch(request, (results, status) => {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (const selectedPlace of results) {
+                        const marker = new google.maps.Marker({
+                            position: selectedPlace.geometry.location,
+                            map: myMap,
+                            title: selectedPlace.name,
+                            icon: "images/marker.png",
+                        })
+                        myMap.setCenter(selectedPlace.geometry.location)
+                        marker.addListener('click', () => {
+                            infowindow.setContent(`<img src='${selectedPlace.photos[0].getUrl({ maxWidth: 250, maxHeight: 200 })}' alt='No image'><br><strong>${selectedPlace.name}</strong><br>${selectedPlace.vicinity}<br>Rating: ${selectedPlace.rating}`);
+                            infowindow.open(myMap, marker);
+                        })
+                    }
+                }
+            })
+        }
+    })
 }
 
 
