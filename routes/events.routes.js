459fb828cc00d5b94fa651c5940fe32a2374ca2b
samuelemarino04 <samuele12601@gmail.com>
2023-08-17 20:55:34 +0200

    donr

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -4,82 +4,78 @@ const Event = require("../models/Event.model")
 const { isLoggedIn, checkRoles } = require('../middlewares/route-guard');
 const uploaderMiddleware = require('../middlewares/uploader')
 const { formatDate, formatTime } = require('../utils/date-utils')
-const geocodingApi = require('../services/geocode.service')
 
 
-router.get("/", (req, res, next) => {
-    let isAdmin = false
+router.get("/", (req, res) => {
+    const loggedUser = req.session.currentUser
+    let isPM = false
     if (req.session.currentUser.role === 'ADMIN') {
-        isAdmin = true
+        isPM = true
     }
     Event
         .find()
         .then(events => {
-            res.render("events/list", { events, isAdmin });
-        })
-        .catch(err => next(err))
+            res.render("events/list", { loggedUser, events, isPM });
+        });
 })
-
 
 router.get("/:id/info", isLoggedIn, (req, res, next) => {
     const { id: event_id } = req.params
     const loggedUser = req.session.currentUser
+    //poner el utils de la fecha y de la ora
 
     Event
         .findById(event_id)
-        .populate('attender')
-        .then(event => {
-            event.formattedDate = formatDate(event.date)
-            event.formattedTime = formatTime(event.date)
-
-            let isJoined = false
-            event.attender.forEach(eachAtt => {
-                if (eachAtt._id.toHexString() === loggedUser._id) {
-                    isJoined = true
-                }
-            })
-            res.render('events/info', { event, isJoined })
-        })
-        .catch(err => next(err))
+        .then(event => res.render('events/info', { loggedUser, event }))
+        .catch(err => console.log(err))
 })
+
 
 
 router.get("/add", isLoggedIn, (req, res) => {
-    res.render("events/addevent")
+    res.render("events/addevent", { loggedUser: req.session.currentUser })
 })
 
 router.post("/add", isLoggedIn, uploaderMiddleware.single('icon'), (req, res, next) => {
-    const { title, icon, description, type, address, date } = req.body
 
-    geocodingApi
-        .getCoordenates(address)
-        .then(response => {
-            const location = {
-                type: 'Point',
-                coordenates: [response.data.results[0].geometry.location.lng, response.data.results[0].geometry.location.lat]
-            }
-            return location
-        })
-        .then(location => Event
-            .create({ title, icon, description, type, address, location, date })
-            .then(() => res.redirect('/events')))
+    const { title, icon, description, type, address, latitude, longitude, date } = req.body
+
+    const newUserData = { title, icon, description, type, address, latitude, longitude, date }
+    // const location = {
+    //     type: 'Point',
+    //     coordinates: [longitude, latitude]
+    // }
+    if (req.file) {
+        const { path: icon } = req.file
+        newUserData.icon = icon
+    }
+
+    Event
+        .create({ title, icon, description, type, address, date })
+        .then(() => res.redirect('/events'))
         .catch(err => next(err))
+
 })
 
 
-router.get("/:id/edit", isLoggedIn, (req, res, next) => {
+router.get("/:id/edit", isLoggedIn, (req, res) => {
     const { id: event_id } = req.params
+
+    const loggedUser = req.session.currentUser
 
     Event
         .findById(event_id)
-        .then(event => res.render("events/editevent", { event }))
+        .then(event => res.render("events/editevent", { loggedUser, event }))
         .catch(err => next(err))
 })
 
-
-router.post("/:id/edit", isLoggedIn, uploaderMiddleware.single('icon'), (req, res, next) => {
+router.post("/:id/edit", isLoggedIn, uploaderMiddleware.single('icon'), (req, res) => {
     const { id: event_id } = req.params
     const { title, icon, description, type, address, latitude, longitude, date } = req.body
+    // const location = {
+    //     type: 'Point',
+    //     coordinates: [longitude, latitude]
+    // }
     const newUserData = { title, icon, description, type, address, location, date }
 
     if (req.file) {
@@ -92,33 +88,15 @@ router.post("/:id/edit", isLoggedIn, uploaderMiddleware.single('icon'), (req, re
         .then(() => res.redirect(`/events/${event_id}/info`))
         .catch(err => next(err))
 })
+//no se guarda la fecha asignada en el create, ni las coordenadas
 
 
-router.get("/:id/delete", isLoggedIn, (req, res, next) => {
+router.get("/:id/delete", isLoggedIn, (req, res) => {
     const { id: event_id } = req.params
 
     Event
         .findByIdAndDelete(event_id)
         .then(() => res.redirect('/events'))
-        .catch(err => next(err))
-})
-
-
-router.get("/:id/join-event", isLoggedIn, (req, res, next) => {
-    const { id: event_id } = req.params
-    const loggedUser = req.session.currentUser
-    Event
-        .findByIdAndUpdate(event_id, { $push: { attender: loggedUser._id } })
-        .then(() => res.redirect(`/events/${event_id}/info`))
-        .catch(err => next(err))
-})
-
-router.get("/:id/unjoin-event", isLoggedIn, (req, res, next) => {
-    const { id: event_id } = req.params
-    const loggedUser = req.session.currentUser
-    Event
-        .findByIdAndUpdate(event_id, { $pull: { attender: loggedUser._id } })
-        .then(() => res.redirect(`/events/${event_id}/info`))
         .catch(err => next(err))
 })
 
